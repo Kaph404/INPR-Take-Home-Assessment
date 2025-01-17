687683294b132c41c0e3a2f62b40c9a7b6854d94
Kaph404 <kaph8858@gmail.com>
2025-01-17 11:28:04 +0000

    Add files via upload

---
+++ Topic2/FezzBus/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBus fizzBus = new FizzBus();
+        fizzBus.doFizzBus(50);
+    }
+}
+
+    boolean truth = false;
+!truth
